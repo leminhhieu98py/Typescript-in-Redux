c359c1d14ef11d7e43de90b0cf8e59b8cb3ffd41
otis <leminhhieu98py@gmail.com>
2022-07-15 11:26:59 +0700

    set up store structure with TS

---
+++ src/store/repositories/action.type.ts
@@ -0,0 +1,5 @@
+export enum REPOSITORIES_ACTION_TYPES {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
---
+++ src/store/repositories/reducer.ts
@@ -0,0 +1,27 @@
+import { Action } from './action';
+import { REPOSITORIES_ACTION_TYPES } from './action.type';
+
+interface RepositoriesState {
+  isLoading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case REPOSITORIES_ACTION_TYPES.SEARCH_REPOSITORIES:
+      return { isLoading: true, error: null, data: [] };
+    case REPOSITORIES_ACTION_TYPES.SEARCH_REPOSITORIES_SUCESS:
+      return { isLoading: false, error: null, data: action.payload };
+    case REPOSITORIES_ACTION_TYPES.SEARCH_REPOSITORIES_ERROR:
+      return { isLoading: false, error: action.payload, data: [] };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
